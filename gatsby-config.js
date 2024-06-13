dbe4e9898b9128c6f79ed8333f7accca69d8fe49
yekaos <kyarribas@gmail.com>
2024-06-13 12:36:52 +0200

    13/06/2024

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,17 +42,3 @@ module.exports = {
     },
   ],
 }
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /react-leaflet|leaflet/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,17 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-leaflet|leaflet/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
